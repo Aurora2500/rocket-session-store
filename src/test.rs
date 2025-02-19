94f0cff17a75f6fc64f2d4819bcc9c59ad415788
Aurora <programming.aurora@gmail.com>
2025-02-19 21:49:44 +0000

    updated dependencies

--- src/test.rs
+++ src/test.rs
@@ -8,13 +8,22 @@ use ::redis::Client as RedisClient;
 use rocket::{
 	get,
 	http::{
+		private::cookie::CookieBuilder,
 		SameSite,
 		Status,
 	},
-	local::blocking::Client,
+	local::{
+		asynchronous::Client as AsyncClient,
+		blocking::Client,
+	},
 	post,
+	response::{
+		self,
+		Responder,
+	},
 	routes,
 	Build,
+	Request,
 	Rocket,
 };
 
@@ -22,8 +31,8 @@ use rocket::{
 use crate::redis::RedisStore;
 use crate::{
 	memory::MemoryStore,
-	CookieConfig,
 	Session,
+	SessionError,
 	SessionResult,
 	SessionStore,
 	Store,
@@ -50,10 +59,49 @@ async fn refresh(session: Session<'_, String>) -> SessionResult<()> {
 	session.touch().await
 }
 
-fn example_rocket<T: 'static>(store: SessionStore<T>) -> Rocket<Build> {
-	rocket::build()
-		.attach(store.fairing())
-		.mount("/", routes![set_name, get_name, remove_name, refresh])
+#[post("/regenerate")]
+async fn regenerate(mut session: Session<'_, String>) -> SessionResult<()> {
+	session.regenerate_token().await
+}
+
+#[post("/regenerate_with_error")]
+async fn regenerate_with_error(mut session: Session<'_, String>) -> Result<(), MyError> {
+	session.regenerate_token().await?;
+	Err(MyError::OtherError)
+}
+
+enum MyError {
+	SessionError(SessionError),
+	OtherError,
+}
+
+impl From<SessionError> for MyError {
+	fn from(e: SessionError) -> Self {
+		Self::SessionError(e)
+	}
+}
+
+impl<'r, 'o: 'r> Responder<'r, 'o> for MyError {
+	fn respond_to(self, request: &'r Request<'_>) -> response::Result<'o> {
+		match self {
+			Self::SessionError(e) => e.respond_to(request),
+			Self::OtherError => Err(Status::InternalServerError),
+		}
+	}
+}
+
+fn example_rocket<T: Send + Sync + Clone + 'static>(store: SessionStore<T>) -> Rocket<Build> {
+	rocket::build().attach(store.fairing()).mount(
+		"/",
+		routes![
+			set_name,
+			get_name,
+			remove_name,
+			refresh,
+			regenerate,
+			regenerate_with_error
+		],
+	)
 }
 
 fn generic_basic_test(store: impl Store<Value = String> + 'static) {
@@ -62,7 +110,7 @@ fn generic_basic_test(store: impl Store<Value = String> + 'static) {
 			store: Box::new(store),
 			name: "token".into(),
 			duration: Duration::from_secs(3600),
-			cookie: CookieConfig::default(),
+			cookie_builder: CookieBuilder::new("", ""),
 		};
 		let rocket = example_rocket(session_store);
 		Client::tracked(rocket).expect("Expected to build client")
@@ -88,7 +136,7 @@ fn generic_expiration_test(store: impl Store<Value = String> + 'static) {
 			store: Box::new(store),
 			name: "token".into(),
 			duration: Duration::from_secs(1),
-			cookie: CookieConfig::default(),
+			cookie_builder: CookieBuilder::new("", ""),
 		};
 		let rocket = example_rocket(session_store);
 		Client::tracked(rocket).expect("Expected to build client")
@@ -109,7 +157,7 @@ fn generic_remove_test(store: impl Store<Value = String> + 'static) {
 			store: Box::new(store),
 			name: "token".into(),
 			duration: Duration::from_secs(3600),
-			cookie: CookieConfig::default(),
+			cookie_builder: CookieBuilder::new("", ""),
 		};
 		let rocket = example_rocket(session_store);
 		Client::tracked(rocket).expect("Expected to build client")
@@ -137,7 +185,7 @@ fn generic_refresh_test(store: impl Store<Value = String> + 'static) {
 			store: Box::new(store),
 			name: "token".into(),
 			duration: Duration::from_secs(2),
-			cookie: CookieConfig::default(),
+			cookie_builder: CookieBuilder::new("", ""),
 		};
 		let rocket = example_rocket(session_store);
 		Client::tracked(rocket).expect("Expected to build client")
@@ -153,18 +201,18 @@ fn generic_refresh_test(store: impl Store<Value = String> + 'static) {
 	assert_eq!(res3.status(), Status::Ok);
 }
 
-fn cookie_config_test(store: impl Store<Value = String> + 'static) {
+fn generic_cookie_config_test(store: impl Store<Value = String> + 'static) {
 	let client: Client = {
 		let session_store: SessionStore<String> = SessionStore {
 			store: Box::new(store),
 			name: "token".into(),
 			duration: Duration::from_secs(2),
-			cookie: CookieConfig {
-				path: Some("/".into()),
-				same_site: Some(SameSite::Lax),
-				secure: true,
-				http_only: true,
-			},
+			cookie_builder: CookieBuilder::new("", "")
+				.path("/")
+				// Rocket defaults to SameSite=Lax, Secure=true, HttpOnly=true, so we test with non-defaults
+				.same_site(SameSite::Strict)
+				.secure(false)
+				.http_only(false),
 		};
 		let rocket = example_rocket(session_store);
 		Client::tracked(rocket).expect("Expected to build client")
@@ -182,9 +230,159 @@ fn cookie_config_test(store: impl Store<Value = String> + 'static) {
 	let cookie = cookie.unwrap();
 
 	assert_eq!(cookie.path(), Some("/"));
-	assert_eq!(cookie.same_site(), Some(SameSite::Lax));
-	assert_eq!(cookie.secure(), Some(true));
-	assert_eq!(cookie.http_only(), Some(true));
+	assert_eq!(cookie.same_site(), Some(SameSite::Strict));
+	assert_eq!(cookie.secure(), None);
+	assert_eq!(cookie.http_only(), None);
+}
+
+fn generic_dont_resend_cookie_test(store: impl Store<Value = String> + 'static) {
+	let client: Client = {
+		let session_store: SessionStore<String> = SessionStore {
+			store: Box::new(store),
+			name: "token".into(),
+			duration: Duration::from_secs(2),
+			cookie_builder: CookieBuilder::new("", ""),
+		};
+		let rocket = example_rocket(session_store);
+		Client::tracked(rocket).expect("Expected to build client")
+	};
+
+	let res1 = client.post("/set_name/TestingName").dispatch();
+	assert_eq!(res1.status(), Status::Ok);
+	// The first request should add a session cookie
+	assert_eq!(res1.cookies().iter().collect::<Vec<_>>().len(), 1);
+
+	// Subsequent requests shouldn't set the cookie
+
+	let res2 = client.post("/set_name/NewName").dispatch();
+	assert_eq!(res2.status(), Status::Ok);
+	assert_eq!(res2.cookies().iter().collect::<Vec<_>>().len(), 0);
+
+	let res3 = client.post("/refresh").dispatch();
+	assert_eq!(res3.status(), Status::Ok);
+	assert_eq!(res3.cookies().iter().collect::<Vec<_>>().len(), 0);
+
+	let res4 = client.get("/get_name").dispatch();
+	assert_eq!(res4.status(), Status::Ok);
+	assert_eq!(res4.cookies().iter().collect::<Vec<_>>().len(), 0);
+}
+
+async fn generic_regenerate_token_test(store: impl Store<Value = String> + 'static) {
+	let client: AsyncClient = {
+		let session_store: SessionStore<String> = SessionStore {
+			store: Box::new(store),
+			name: "token".into(),
+			duration: Duration::from_secs(2),
+			cookie_builder: CookieBuilder::new("", ""),
+		};
+		let rocket = example_rocket(session_store);
+		AsyncClient::tracked(rocket)
+			.await
+			.expect("Expected to build client")
+	};
+
+	let res1 = client.post("/set_name/TestingName").dispatch().await;
+	assert_eq!(res1.status(), Status::Ok);
+	let res1_cookies = client.cookies();
+	let original_token = res1_cookies.get("token").unwrap();
+	assert_eq!(
+		client
+			.rocket()
+			.state::<SessionStore<String>>()
+			.unwrap()
+			.store
+			.get(original_token.value())
+			.await
+			.unwrap(),
+		Some("TestingName".into())
+	);
+
+	let res2 = client.post("/regenerate").dispatch().await;
+	assert_eq!(res2.status(), Status::Ok);
+	assert_eq!(
+		client
+			.rocket()
+			.state::<SessionStore<String>>()
+			.unwrap()
+			.store
+			.get(original_token.value())
+			.await
+			.unwrap(),
+		None
+	);
+	let res2_cookies = client.cookies();
+	let new_token = res2_cookies.get("token").unwrap();
+	assert_ne!(original_token, new_token);
+	assert_eq!(
+		client
+			.rocket()
+			.state::<SessionStore<String>>()
+			.unwrap()
+			.store
+			.get(new_token.value())
+			.await
+			.unwrap(),
+		Some("TestingName".into())
+	);
+}
+
+async fn generic_regenerate_token_with_error_test(store: impl Store<Value = String> + 'static) {
+	let client: AsyncClient = {
+		let session_store: SessionStore<String> = SessionStore {
+			store: Box::new(store),
+			name: "token".into(),
+			duration: Duration::from_secs(2),
+			cookie_builder: CookieBuilder::new("", ""),
+		};
+		let rocket = example_rocket(session_store);
+		AsyncClient::tracked(rocket)
+			.await
+			.expect("Expected to build client")
+	};
+
+	let res1 = client.post("/set_name/TestingName").dispatch().await;
+	assert_eq!(res1.status(), Status::Ok);
+	let res1_cookies = client.cookies();
+	let original_token = res1_cookies.get("token").unwrap();
+	assert_eq!(
+		client
+			.rocket()
+			.state::<SessionStore<String>>()
+			.unwrap()
+			.store
+			.get(original_token.value())
+			.await
+			.unwrap(),
+		Some("TestingName".into())
+	);
+
+	let res2 = client.post("/regenerate_with_error").dispatch().await;
+	assert_eq!(res2.status(), Status::InternalServerError);
+	assert_eq!(
+		client
+			.rocket()
+			.state::<SessionStore<String>>()
+			.unwrap()
+			.store
+			.get(original_token.value())
+			.await
+			.unwrap(),
+		None
+	);
+	let res2_cookies = client.cookies();
+	let new_token = res2_cookies.get("token").unwrap();
+	assert_ne!(original_token, new_token);
+	assert_eq!(
+		client
+			.rocket()
+			.state::<SessionStore<String>>()
+			.unwrap()
+			.store
+			.get(new_token.value())
+			.await
+			.unwrap(),
+		Some("TestingName".into())
+	);
 }
 
 macro_rules! test_store {
@@ -213,8 +411,23 @@ macro_rules! test_store {
 			}
 
 			#[test]
-			fn cookie_test() {
-				cookie_config_test($store);
+			fn cookie_config_test() {
+				generic_cookie_config_test($store);
+			}
+
+			#[test]
+			fn dont_resend_cookie_test() {
+				generic_dont_resend_cookie_test($store);
+			}
+
+			#[rocket::async_test]
+			async fn regenerate_token_test() {
+				generic_regenerate_token_test($store).await;
+			}
+
+			#[rocket::async_test]
+			async fn regenerate_token_with_error_test() {
+				generic_regenerate_token_with_error_test($store).await;
 			}
 		}
 	};
